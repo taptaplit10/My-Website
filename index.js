c936cb6d617115ee312368105280de57449ef309
taptaplit10 <65609468+taptaplit10@users.noreply.github.com>
2020-05-26 19:03:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-alert("Welcome")
+function showTime(){
+  let date = new Date();
+  let hours = date.getHours();
+  let mins = date.getMinutes();
+  let seconds = date.getSeconds();
+  let session = 'AM';
+  
+  
+  if(hours == 0) {
+    hours = 12;
+  }
+  
+  if(hours > 12){
+    hours = hours - 12;
+    session = 'PM';
+  }
+  
+  hours = (hours < 10) ? "0" + hours : hours;
+  mins = (mins < 10) ? "0" + mins : mins;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+  
+  let time = hours + ":" + mins + ":" + seconds + " " + session;
+  
+  let MyClockDisplay = document.getElementById('MyClockDisplay');
+  MyClockDisplay.innerHTML = time;
+  setTimeout(showTime, 1000)
+}
+
+
+showTime()
